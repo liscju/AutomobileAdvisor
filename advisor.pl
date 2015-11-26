aeb84edc21c1dbae699f66b5dea31948f5c9e45b
liscju <piotr.listkiewicz@gmail.com>
2015-11-26 16:34:31 +0100

    Remove old predicates checking type of car

--- advisor.pl
+++ advisor.pl
@@ -1,24 +1,5 @@
 :- module(advisor, []).
 
-match_sport_car(X) :-
-	is_single(X),
-	is_self_confident(X).
-
-match_city_car(X) :-
-	is_single(X),
-	needs_car_for_city(X).
-
-match_family_compact_car(X) :-
-	not(is_single(X)),
-	has_small_family(X).
-
-match_family_mid_size_car(X) :-
-	not(is_single(X)),
-	has_medium_family(X).
-
-is_sport_car(X) :-
-	has_max_velocity_above(X,450),
-	has_seats(X,2).
 
 has_cost(fiat_125p,2000).
 has_cost(fiat_126p,1200).
